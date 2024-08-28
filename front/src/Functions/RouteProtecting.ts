2075e4760a0abc2b4a1b9f4fae2e0077669ff83f
Suphakit P <me@suphakit.net>
2024-08-28 15:19:21 +0700

    feat<function>: added route protect function

--- front/src/Functions/RouteProtecting.ts
+++ front/src/Functions/RouteProtecting.ts
@@ -1,1 +1,33 @@
-export function RouteProtection() {}
+import { NavigateFunction } from "react-router-dom";
+
+export function RouteProtection(
+  navigate: NavigateFunction,
+  salt: string,
+  expired: number
+) {
+  const fetchConfig = {
+    url: `${import.meta.env.VITE_BE_URI}/api/auth/check`,
+    config: {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${salt}`,
+      }),
+    },
+  };
+
+  if (Number(new Date().getTime()) >= Number(expired)) {
+    sessionStorage.clear();
+    navigate("/");
+  } else {
+    fetch(fetchConfig.url, fetchConfig.config)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.status == 0) {
+          navigate("/");
+        } else {
+          navigate("/admin");
+        }
+      });
+  }
+}

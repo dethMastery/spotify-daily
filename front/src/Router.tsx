77bd500843486faf9cce13a627a706291e94c030
Suphakit P <me@suphakit.net>
2024-08-30 10:47:34 +0700

    feat<routes>: added history route

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -3,21 +3,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ErrorPage } from "./Pages/Errors";
 
 import { HomePage } from "./Pages/Home";
+import { HistoryPage } from "./Pages/History";
 
 import { CallbackPage } from "./Pages/Callback";
 import { LoginPage } from "./Pages/Login";
+import { Logout } from "./Pages/Logout";
 import { AdminPage } from "./Pages/Admin";
 
 import { SpotifyLoginPage } from "./Pages/Spotify/Login";
 import { SpotifyCallbackPage } from "./Pages/Spotify/Callback";
 import { SpotifyLogoutPage } from "./Pages/Spotify/Logout";
-import { Logout } from "./Pages/Logout";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/history" element={<HistoryPage />} />
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/auth/callback" element={<CallbackPage />} />

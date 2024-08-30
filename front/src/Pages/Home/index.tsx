084e590af3a1592ce49377e58516fdf4f03515c2
Suphakit P <me@suphakit.net>
2024-08-30 11:01:32 +0700

    feat<index>: album cover as bg

--- front/src/Pages/Home/index.tsx
+++ front/src/Pages/Home/index.tsx
@@ -14,61 +14,66 @@ export const HomePage = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
-      <div className="max-w-[22rem] min-w-[18rem] md:max-w-[40rem] w-full flex flex-col md:!flex-row bg-whitesmoke rounded-xl">
-        <div
-          className={`w-full p-4 text-jet ${
-            latest != undefined ? "" : "hidden"
-          }`}
-        >
-          <div className="my-4 md:block hidden">
-            <h1 className="text-3xl">เพลงหมาประจำช่วงนี้ของนายคัตสึรางิ~</h1>
-            <p className="">
-              อัพเดทล่าสุดเมื่อ :{" "}
-              {latest != undefined
-                ? DateSlug(new Date(latest?.dateAdd))
-                : "Error"}
-            </p>
-          </div>
+    <div
+      style={{ background: `url(${latest?.songImage})` }}
+      className="!bg-center !bg-no-repeat !bg-cover w-full h-screen"
+    >
+      <div className="w-full h-screen flex flex-col items-center justify-center gap-4 bg-overlay backdrop-blur-md">
+        <div className="max-w-[22rem] min-w-[18rem] md:max-w-[40rem] w-full flex flex-col md:!flex-row bg-whitesmoke rounded-xl">
+          <div
+            className={`w-full p-4 text-jet ${
+              latest != undefined ? "" : "hidden"
+            }`}
+          >
+            <div className="my-4 md:block hidden">
+              <h1 className="text-3xl">เพลงหมาประจำช่วงนี้ของนายคัตสึรางิ~</h1>
+              <p className="">
+                อัพเดทล่าสุดเมื่อ :{" "}
+                {latest != undefined
+                  ? DateSlug(new Date(latest?.dateAdd))
+                  : "Error"}
+              </p>
+            </div>
 
-          <iframe
-            style={{ borderRadius: "12px" }}
-            src={`https://open.spotify.com/embed/track/${latest?.songID}?utm_source=generator`}
-            className="w-full"
-            height="352"
-            frameBorder="0"
-            allowFullScreen
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-          ></iframe>
+            <iframe
+              style={{ borderRadius: "12px" }}
+              src={`https://open.spotify.com/embed/track/${latest?.songID}?utm_source=generator`}
+              className="w-full"
+              height="352"
+              frameBorder="0"
+              allowFullScreen
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
 
-          <div className="my-6 md:hidden block">
-            <h1 className="text-3xl">เพลงหมาประจำช่วงนี้~</h1>
-            <p className="">
-              อัพเดทล่าสุดเมื่อ :{" "}
-              {latest != undefined
-                ? DateSlug(new Date(latest?.dateAdd))
-                : "Error"}
-            </p>
-          </div>
+            <div className="my-6 md:hidden block">
+              <h1 className="text-3xl">เพลงหมาประจำช่วงนี้~</h1>
+              <p className="">
+                อัพเดทล่าสุดเมื่อ :{" "}
+                {latest != undefined
+                  ? DateSlug(new Date(latest?.dateAdd))
+                  : "Error"}
+              </p>
+            </div>
 
-          <div className="md:mb-2 md:mt-4 my-2 text-center">
-            อยากดูเพลงเก่าๆ หรอ{" "}
-            <a
-              href="/history"
-              className="underline hover:no-underline hover:opacity-80"
-            >
-              กดตรงนี้สิ~
-            </a>
+            <div className="md:mb-2 md:mt-4 my-2 text-center">
+              อยากดูเพลงเก่าๆ หรอ{" "}
+              <a
+                href="/history"
+                className="underline hover:no-underline hover:opacity-80"
+              >
+                กดตรงนี้สิ~
+              </a>
+            </div>
           </div>
+          <div
+            className={`w-full p-4 text-jet ${
+              latest != undefined ? "hidden" : ""
+            }`}
+          ></div>
         </div>
-        <div
-          className={`w-full p-4 text-jet ${
-            latest != undefined ? "hidden" : ""
-          }`}
-        ></div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 };

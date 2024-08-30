4df1321f56622e43a8e3365eaacccb08ff11c55a
Suphakit P <me@suphakit.net>
2024-08-30 11:02:08 +0700

    feat<error>: added error handling page

--- front/src/Pages/Errors/index.tsx
+++ front/src/Pages/Errors/index.tsx
@@ -1,3 +1,17 @@
 export const ErrorPage = () => {
-  return <></>;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <p className="text-5xl mb-4">Not Found</p>
+      <hr className="!w-[10rem]" />
+      <p className="mt-4 text-2xl">There's nothing here :(</p>
+      <button
+        onClick={() => {
+          window.history.back();
+        }}
+        className="px-6 py-4 bg-living-coral my-4 rounded hover:opacity-80 hover:scale-90"
+      >
+        &lt; Go Back
+      </button>
+    </div>
+  );
 };
